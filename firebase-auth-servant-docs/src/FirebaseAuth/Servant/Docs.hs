8a6100103cadfb68432743578d735122b494d026
Thomas Bach <info@thomasbach.dev>
2021-10-20 22:29:07 +0200

    feat(docs): provide api docs for optional firebase authentication

--- firebase-auth-servant-docs/src/FirebaseAuth/Servant/Docs.hs
+++ firebase-auth-servant-docs/src/FirebaseAuth/Servant/Docs.hs
@@ -21,3 +21,17 @@ instance Docs.HasDocs api => Docs.HasDocs (AuthServant.FirebaseAuth :> api) wher
         , "where the value starts with `Bearer: ` followed by a JWT token;"
         , "the token has to be issued by Google Firebase;"
         ]
+
+instance Docs.HasDocs api => Docs.HasDocs (AuthServant.OptionalFirebaseAuth :> api) where
+  docsFor _ (endpoint, action) = Docs.docsFor (Proxy @api) (endpoint, action')
+    where
+      action' = action & Docs.authInfo %~ (|> info)
+      info = Docs.DocAuthentication intro reqData
+      intro = "This endpoint can be used with optional authentication!"
+      reqData = unwords
+        [ "- an optional `Authorization` header"
+        , "where the value starts with `Bearer: ` followed by a JWT token;"
+        , "the token has to be issued by Google Firebase;"
+        , "if the authorization header is present, but invalid,"
+        , "the endpoint will return a 401;"
+        ]
