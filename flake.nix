a936b9c41a4653e78b08e7a1ee3a18edf2065eaf
Thomas Bach <info@thomasbach.dev>
2021-12-10 06:21:43 +0100

    build: update nix machinery for NixOS 21.11

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   description = "A very basic flake";
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix/3aa5ffc0afef728894a08c33a06029997cfd3872";
+    flake-utils.url = "github:numtide/flake-utils/74f7e4319258e287b0f9cb95426c9853b282730b";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  };
   outputs = { self, nixpkgs, flake-utils, haskellNix }:
     flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
       let
